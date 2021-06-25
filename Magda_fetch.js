3297bc88bac68b56b0d14effaee1555fb2f18f4e
MagdalenaES83 <84572667+MagdalenaES83@users.noreply.github.com>
2021-06-25 15:02:31 +0200

    Update Magda_fetch.js

--- Magda_fetch.js
+++ Magda_fetch.js
@@ -1,23 +1,62 @@
 
 let albumList ;
 
+/* Murino methode 
+function firstBtn() {
+    fetch(
+      "https://striveschool-api.herokuapp.com/api/deezer/search?q=queen",
+      {
+        method: "GET",
+      }
+    )
+      .then((resp) => resp.json())
+      .then(({ data }) => {
+        let displaySongs = document.querySelector(".main-container-two");
+        data.forEach((titles) => {
+          console.log("dentro foreach");
+          displaySongs.insertAdjacentHTML(
+            "beforeend",
+            
+            <div class="col d-none d-sm-block">
+              <div class="card h-100">
+                <div class="position-relative">
+                  <img
+                    src="${titles.album.cover_medium}"
+                    class="card-img-top img-fluid rounded py-3 px-2"
+                    alt="..."
+                  />
+
+                  <div class="play"></div>
+                </div>
+
+                <div class="card-body px-2 pt-0">
+                  <h5 class="card-title mb-1 text-white">
+                   ${titles.artist.name}
+                  </h5>
+                  <p class="card-text text-white-50">
+                    Artist
+                  </p>
+                </div>
+              </div>
+            </div>
+          
+          );
+        });
+      });
+  }
+
+  firstBtn()
+*/
+
+
 window.onload =  () => {
     fetchQueen()
     fetchAlbum("75621062")
     getAlbum()
     }
 
-
-
-const getAlbum = async () => {
-        let data = await requestAlbum()    
-        albumList = data
-        displayAlbums()
-}
-
-
-
 //fetch deadEnd for Queen and display in console
+
     const fetchQueen = function () {
         fetch ("https://striveschool-api.herokuapp.com/api/deezer/album/75621062 ",  {
             method:"GET"
@@ -31,63 +70,42 @@ const getAlbum = async () => {
 
 
 //--------------------------------------------fetch albums  and display in console
-const fetchAlbum = function (id) {
-    fetch ("https://striveschool-api.herokuapp.com/api/deezer/album/ " + id,  {
+const fetchAlbum = function () {
+    fetch ("https://striveschool-api.herokuapp.com/api/deezer/album/75621062 " ,  {
         method:"GET"
     })
-    .then(resp => { 
+    .then((resp) => { 
         return resp.json()
     })
-    .then(data => 
-
-//replace card with albums data CREATE A GRIP inside of fetchAlbum function
-
-        function AlbumHTML(id, cover_small, artist, title){
-        return`
-            <div class="col pl-0">
+    .then(({ data} )  =>  {
+        console.log(data);
+        let displayAlbums = document.querySelector(".row-display");
+            data.forEach((titles) => {
+            
+            displayAlbums.insertAdjacentHTML("beforeend",   
+            `                <div class="col pl-0">
                   <div class="card h-100">
                     <div class="position-relative">
                       <img
-                        src="${cover_small}"
+                        src="${titles.album.cover_medium}"
                         class="card-img-top img-fluid rounded py-3 px-2"
                         alt="..."           />
-
-                      <div class="play">${artist}
+                      <div class="play">${titles.artist.name}
                       </div>
                     </div>
-
                     <div class="card-body px-2 pt-0">
                       <h5 class="card-title mb-1 text-white">
-                        ${title}
+                        ${titles.title}
                       </h5>
-                      <p class="card-text text-white-50">${id}</p>
+                      <p class="card-text text-white-50">${titles.id}</p>
                     </div>
                   </div>
                 </div> `
+           )
 }
-         )
+)
+    }
+    )
 }
+*/
 
-
-
-
-
-/* const array1 = ['a', 'b', 'c'];
-
-array1.forEach(element => console.log(element));*/
-
-function displayAlbums(){
-    let section = document.querySelectorAll('.h-100')
-
-    albumList.forEach(album => {
-        section[0].insertAdjacentHTML('beforeend', `${AlbumHTML(album.title, album.artist, album.id, album.cover_small, )}`)
-    });
- 
-}
-
-
-
-
-
-
-    
